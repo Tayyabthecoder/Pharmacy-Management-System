01e438285dd506cf7de0d2da0e4c4c4c63e9c45f
Muhammad Tayyab <wamtayyab2@gmail.com>
2026-09-01 23:03:07 +0500

    Remove database dumps, migrations, and sessions from git tracking

--- database/dumps/add_print_template_setting.sql
+++
@@ -1,6 +0,0 @@
--- Migration: Add print_template system setting key
--- Default value is 'simple'
-
-INSERT INTO system_settings (meta_key, meta_value) 
-VALUES ('print_template', 'simple')
-ON DUPLICATE KEY UPDATE meta_value = VALUES(meta_value);
